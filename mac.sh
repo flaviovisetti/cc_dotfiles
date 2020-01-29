c75e99dcfdddbd70072b64f5dcf9ce2cacb7e1d0
Flávio Visetti <flavio.visetti@gmail.com>
2020-01-29 20:04:44 -0300

    Change install command of macvim

--- mac.sh
+++ mac.sh
@@ -3,7 +3,7 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap Goles/battery
 brew install zsh ctags git hub tmux reattach-to-user-namespace the_silver_searcher battery spark
-brew install macvim --custom-icons --override-system-vim --with-lua --with-luajit
+brew install macvim
 brew cask install google-chrome
 brew cask install iterm2
 
